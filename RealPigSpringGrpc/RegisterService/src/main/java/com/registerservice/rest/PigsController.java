531242e71c3135fb457f9bf32b2765e7fe21c6ed
Simon S.C <simon.sohn.privat@gmail.com>
2023-01-18 12:21:22 +0100

    REST http
    Controller metoder sat op for de to nyde database egenskaber get origin / get date

--- RealPigSpringGrpc/RegisterService/src/main/java/com/registerservice/rest/PigsController.java
+++ RealPigSpringGrpc/RegisterService/src/main/java/com/registerservice/rest/PigsController.java
@@ -4,10 +4,11 @@ import com.domain.Pig;
 import com.persistence.RegisterDataAccess;
 import com.domain.dtos.RegisterPigDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -19,5 +20,20 @@ public class PigsController {
 	@PostMapping("/pigs")
 	public Pig registerPig(@RequestBody RegisterPigDto pig) {
 		return registerDataAccess.registerPig(pig);
+	}
+
+
+
+	//Kunne godt være disse metoder skulle være i andet interface end registerDataAcces eller ændre nuværende interface til noget lignende data management.  ¯\_(ツ)_/¯.
+	@GetMapping("/pigs/arrival")
+	public List<Pig> getPigs(@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate date)
+	{
+		return registerDataAccess.getFromDate(date);
+	}
+
+	@GetMapping("/pigs/farm")
+	public List<Pig> getPigs(@RequestParam String origin)
+	{
+		return registerDataAccess.getFromOrigin(origin);
 	}
 }
--- RealPigSpringGrpc/persistence/src/main/java/com/persistence/RegisterDataAccess.java
+++ RealPigSpringGrpc/persistence/src/main/java/com/persistence/RegisterDataAccess.java
@@ -8,9 +8,14 @@ import com.domain.dtos.RegisterProductDto;
 import com.domain.dtos.RegisterTrayDto;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface RegisterDataAccess {
 		Product registerProduct(RegisterProductDto product);
 		Pig registerPig(RegisterPigDto pig);
 		Tray registerTray(RegisterTrayDto tray);
+		List<Pig> getFromOrigin(String origin);
+		List<Pig> getFromDate(LocalDate date);
 }
